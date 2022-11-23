f84ddcb6820bcee39566a0a4e0a76bbfb21ca1ad
kcw2297 <chaeugg@gmail.com>
2022-11-23 11:31:36 +0000

    (fix) create-미완성 2022-11-23

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -76,13 +76,13 @@ syscall_handler (struct intr_frame *f UNUSED) {
 	// 	wait();
 	// 	break;
 
-	// case SYS_CREATE:
-	// 	create();
-	// 	break;
+	case SYS_CREATE:
+		create(f->R.rdi, f->R.rsi);
+		break;
 	
-	// case SYS_REMOVE:
-	// 	remove();
-	// 	break;
+	case SYS_REMOVE:
+		remove(f->R.rdi);
+		break;
 
 	// case SYS_OPEN:
 	// 	open();
@@ -122,11 +122,11 @@ syscall_handler (struct intr_frame *f UNUSED) {
 void check_address(void *addr)
 {
 	/* 포인터가 가리키는 주소가 유저영역의 주소인지 확인 */
-	if (!is_user_vaddr(addr))
+	if (!is_user_vaddr(addr) || addr == NULL)
 	{
 		/* 잘못된 접근일 경우 프로세스 종료 */
 		/* 확인 요망 */
-		process_exit();
+		exit(-1);
 	}
 }
 
@@ -199,7 +199,7 @@ int wait(pid_t pid)
 
 bool create(const char* file, unsigned initial_size)
 {
-	check_address(&file);
+	check_address(file);
 	/* 파일 이름과 크기에 해당하는 파일 생성 */
 	/* 파일 생성 성공 시 true 반환, 실패 시 flase 반환 */
 	return (filesys_create(file, initial_size)) ? true : false;
@@ -207,7 +207,7 @@ bool create(const char* file, unsigned initial_size)
 
 bool remove(const char *file)
 {
-	check_address(&file);
+	check_address(file);
 	/* 파일 이름에 해당하는 파일을 제거 */
 	/* 파일 제거 성공 시 true 반환, 실패 시 false 반환 */
 	return (filesys_remove(file)) ? true : false;
